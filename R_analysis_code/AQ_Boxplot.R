b264f9d6db7dbed6f30545ae9034dbfd057fb768
lizadams <lizadams@email.unc.edu>
2026-06-25 18:44:26 +0000

    update

--- R_analysis_code/AQ_Boxplot.R
+++ R_analysis_code/AQ_Boxplot.R
@@ -316,6 +316,14 @@ if (inc_whiskers == 'y') {
 }
 bias.y.axis.max <- bias.y.axis.max.value+((bias.y.axis.max.value-bias.y.axis.min)*0.25)
 
+norm_bias.x.axis.min <- min(q1.norm_bias,q1.norm_bias2,q1.norm_bias3)
+norm_bias.x.axis.max.value <- max(q1.norm_bias,q1.norm_bias2,q1.norm_bias3)
+if (inc_whiskers == 'y') {
+   norm_bias.x.axis.min <- min(Norm_Bias,Norm_Bias2,Norm_Bias3)                              # Set y-axis minimum values
+   norm_bias.x.axis.max.value <- max(Norm_Bias,Norm_Bias2,Norm_Bias3)                        # Determine y-axis maximum value
+}
+norm_bias.x.axis.max <- norm_bias.x.axis.max.value+((norm_bias.x.axis.max.value-norm_bias.x.axis.min)*0.25)
+
 norm_bias.y.axis.min <- min(q1.norm_bias,q1.norm_bias2,q1.norm_bias3)
 norm_bias.y.axis.max.value <- max(q3.norm_bias,q3.norm_bias2,q3.norm_bias3)
 if (inc_whiskers == 'y') {
